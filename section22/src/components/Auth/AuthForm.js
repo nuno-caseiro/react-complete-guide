e95bc6967a4b955cca4a1ffea027385c350b04fa
Nuno Caseiro <n.caseiro@kigrou.de>
2021-09-15 18:17:41 +0100

    section 22 almost finished

--- section22/src/components/Auth/AuthForm.js
+++ section22/src/components/Auth/AuthForm.js
@@ -1,34 +1,107 @@
-import { useState } from 'react';
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router";
+import AuthContext from "../../store/auth-context";
 
-import classes from './AuthForm.module.css';
+
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const history = useHistory()
+
+  const authCtx = useContext(AuthContext)
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(isLogin)
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+    
+    setIsLoading(true);
+    console.log(isLogin)
+    let url;
+    if (!isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB_KLKuxvv3xMGjeDcbQDKt0XZevXBUsk0";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB_KLKuxvv3xMGjeDcbQDKt0XZevXBUsk0";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+           res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            //  if (data && data.error && data.error.message) {
+            //    errorMessage = data.error.message;
+            //  }
+
+            return new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCtx.login(data.idToken)
+        history.replace('/')
+      })
+      .catch((err) => {
+        console.log("HI")
+        alert(err.message);
+      });
+  };
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
